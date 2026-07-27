afda467aeb301e6cda772a1afdb7b50caa95c178
PiersonW <39450417+PiersonW@users.noreply.github.com>
2026-07-27 17:03:40 -0600

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -10,13 +10,7 @@ export default function Navbar() {
     <nav className="nav">
       <div className="nav-inner">
         <Link href="/" className="brand">
-          <svg className="brand-mark" viewBox="0 0 24 24" fill="none">
-            <path
-              d="M2 19L8 8L11.5 14L14 10L22 19H2Z"
-              fill="var(--crust)"
-            />
-          </svg>
-          Alpine Bakery
+          <img src="/logo.png" alt="Alpine Bakery" className="brand-logo" />
         </Link>
         <div className="nav-links">
           <Link href="/">Home</Link>
